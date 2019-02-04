45eb407a01d72a1195ce80a68d9be0e616bbb281
SpiralArm Consulting Ltd <projects@spiralarm.uk>
2019-02-04 16:06:12 +0000

    updates for 0.0.1 release

--- lib/polylingual.dart
+++ lib/polylingual.dart
@@ -2,7 +2,7 @@
 // is governed by an Apache License 2.0 that can be found in the LICENSE file.
 library polylingual;
 
-/// PolyLingual is a plug-in that can be used to provide multiple language translation strings for an app
+/// [PolyLingual] is a plug-in that can be used to provide multiple language translation strings for an app
 /// It is a static class with the following methods
 /// 
 /// [initialise]
